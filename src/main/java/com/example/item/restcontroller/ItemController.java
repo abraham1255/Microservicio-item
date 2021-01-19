329e24b79c76da2941f980d416ad5735385df3ef
OscarRamirez94 <al221310583@gmail.com>
2021-01-18 19:04:22 -0600

    Se agrega actuator

--- src/main/java/com/example/item/restcontroller/ItemController.java
+++ src/main/java/com/example/item/restcontroller/ItemController.java
@@ -1,10 +1,19 @@
 package com.example.item.restcontroller;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,26 +24,47 @@ import com.example.item.models.Producto;
 import com.example.item.service.ItemService;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@RefreshScope
 @RestController
 public class ItemController {
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+
+	@Value("${configuracion.text}")
+	private String texto;
 	
-@Autowired
-@Qualifier("itemServiceFeing")
+
+	@Autowired
+	private Environment env;
+	
+	@Autowired
+	@Qualifier("itemServiceFeing")
 //@Qualifier("serviceRestTemplate")
-private IItemService itemService;
+	private IItemService itemService;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
+
 	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/detalle/{id}/cantidad/{cantidad}")
-	public Item detalle (@PathVariable Long id, @PathVariable Integer cantidad) {
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findaById(id, cantidad);
 	}
 	
-	public Item metodoAlternativo (Long id,  Integer cantidad) {
+
+	@GetMapping("/obtenerConfig")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		log.info(texto);
+		Map<String,String> map = new HashMap<String,String>();
+		map.put("texto", texto);
+		map.put("puerto",puerto);
+		
+		
+		return new ResponseEntity<Map<String,String>>(map,HttpStatus.OK);
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
 		item.setCantidad(cantidad);
