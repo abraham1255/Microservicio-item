de62d399fef4231fa0ad68df9a004b5e59976742
Abraham <root@localhost.localdomain>
2021-12-21 20:27:57 -0600

    version prueba

--- src/main/java/com/example/item/restcontroller/ItemController.java
+++ src/main/java/com/example/item/restcontroller/ItemController.java
@@ -21,9 +21,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.item.iservice.IItemService;
-import com.example.item.models.Item;
-import com.example.commons.models.Producto;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RefreshScope
@@ -42,19 +39,6 @@ public class ItemController {
 	@Autowired
 	@Qualifier("itemServiceFeign")
 	//@Qualifier("serviceRestTemplate")
-	private IItemService itemService;
-
-	@GetMapping("/listar")
-	public List<Item> listar() {
-		return itemService.findAll();
-	}
-
-	@HystrixCommand(fallbackMethod = "metodoAlternativo")
-	@GetMapping("/detalle/{id}/cantidad/{cantidad}")
-	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return itemService.findaById(id, cantidad);
-	}
-	
 
 	@GetMapping("/obtenerConfig")
 	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
@@ -66,34 +50,4 @@ public class ItemController {
 		
 		return new ResponseEntity<Map<String,String>>(map,HttpStatus.OK);
 	}
-
-	public Item metodoAlternativo(Long id, Integer cantidad) {
-		Item item = new Item();
-		Producto producto = new Producto();
-		item.setCantidad(cantidad);
-		producto.setId(id);
-		producto.setNombre("camara sony");
-		producto.setPrecio(10000.00);
-		item.setProducto(producto);
-		return item;
-	}
-	
-	@PostMapping("/crear")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Producto crear(@RequestBody Producto producto) {
-		return itemService.save(producto);
-	}
-	
-	@PutMapping("/editar/{id}")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
-		return itemService.update(producto, id);
-	}
-	
-	@DeleteMapping("/eliminar/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void eliminar(@PathVariable Long id) {
-		itemService.delete(id);
-	}
-
 }
